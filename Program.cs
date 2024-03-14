8697be2c35cebef2e32b50c15c7818e151b2a280
HarunCelen <haruncelen@yahoo.com>
2024-03-15 01:18:54 +0300

    Otomata_sorusu

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Algoritma_1
+{
+    internal class Program
+    {
+        static void Varmı(string a, int i, string ifade)
+        {
+
+
+            if (i < ifade.Length)
+            {
+                if (a[i] == ifade[i])
+                {
+                    Console.WriteLine("Otomat q" + (i + 1) + " gidiyor");
+                    Varmı(a, i + 1, ifade);
+
+
+
+                }
+                else
+                {
+                    Console.WriteLine("Otomat q5 gidiyor");
+                }
+
+
+
+
+            }
+
+
+
+
+        }
+        static void Main(string[] args)
+        {
+            Varmı("abba", 0, "abba");
+
+
+        }
+    }
+}
